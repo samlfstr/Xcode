4fec63bb7fd90f678b5b80fdd9f38ded05f402e1
Aaron <samlfstr@gmail.com>
2022-10-15 23:38:09 +0300

    Nineth Project

--- #7Sevent Project/Xylophone/ViewController.swift
+++ #7Sevent Project/Xylophone/ViewController.swift
@@ -7,15 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    func playSound(fileName:String) {
+        let url = Bundle.main.url(forResource: "\(fileName)", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
+    
+    @IBAction func CButton(_ sender: UIButton) {
+        
+        playSound(fileName: sender.currentTitle!)
+        
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.7
+        
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+    }
 }
 
