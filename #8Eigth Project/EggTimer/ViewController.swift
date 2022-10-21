3e85af3636625de4405aeda7cdb646572a5ae8f7
Aaron <samlfstr@gmail.com>
2022-10-21 14:47:38 +0300

    Tenth Project

--- #8Eigth Project/EggTimer/ViewController.swift
+++ #8Eigth Project/EggTimer/ViewController.swift
@@ -8,8 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController {//0
     
-
-
-}
+    var timerr : Timer?
+    
+    @IBOutlet weak var progressViewBar: UIProgressView!
+    var counter = 30
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) { //1
+        // It count seconds
+        var timeInterval = Double(sender.currentTitle!)! * 60
+        let timerInt = timeInterval
+        self.timerr = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { //2
+            timer in
+            
+            if timeInterval == 0 {
+                self.timerr?.invalidate()
+                // Create the alert
+                let alertController = UIAlertController(
+                    title: "Message",
+                    message: "Your Egg is Ready!",
+                    preferredStyle: .alert)
+                
+                // Create OK button
+                let OKAction = UIAlertAction(
+                    title: "OK",
+                    style: .default) {
+                    (action: UIAlertAction!) in
+                    
+                    self.progressViewBar.progress = 0.0
+                    
+                };alertController.addAction(OKAction)
+                
+                // Present Dialog message
+                self.present(alertController, animated: true, completion: nil)
+             
+            }
+            else
+            {
+                // Progress View
+                self.progressViewBar.progress += Float(100 / timerInt * 0.01)
+                timeInterval -= 1
+            }
+        }//2
+        
+        
+    } //1
+    
+    
+    
+    
+    
+}//0
